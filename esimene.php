8eb4852d0c34593822ad5584705fcefe5dc50c48
Kaspar Aasmäe <kaspar@greeny.cs.tlu.ee>
2017-09-14 11:34:16 +0300

    lisatud muutuja ning IF lause

--- esimene.php
+++ esimene.php
@@ -1,3 +1,24 @@
+<?php
+	//muutujad
+	$myName = "Kaspar";
+	$myFamilyName = "Aasmäe";
+	
+	//hindan päeva osa (võrdlemine < > <= >= == != )
+	$hourNow =date("H");
+	$partOfDay = "";
+	if ($hourNow < 8){
+		$partOfDay = "varajne hommik";
+		}
+		if ($hourNow >= 8 and $hourNow < 16) {
+			$partOfDay = "koolipäev";
+		}
+		if ($hourNow > 16) {
+			$partOfDay = "vaba aeg";
+		}
+		//echo partOfDay;
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,13 +26,13 @@
 	<title>Kaspar Aasmä programeerib veebi</title>
 </head>
 <body>
-	<h1>Kaspar Aasmä veebiprogrammeerimine<h1>
+	<h1><?php echo $myName ." " .$myFamilyName; ?>, veebiprogrammeerimine<h1>
 	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
 	<?php
 	echo "<p>Algas PHP õmine.</p>";
 	echo "<p>Täna on ";
 	echo date("d.m.Y") .", kell oli lehe avamise hetkel " .date("H:i:s");
-	echo ".</p>";
+	echo ", hetkel on " $partOfDay .".</p>";
 	?>
 </body>
 </html>
