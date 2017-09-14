7812a6b3020b95d8ae70316aaee9919d0718c807
Kaspar Aasmäe <kaspar@greeny.cs.tlu.ee>
2017-09-14 10:50:09 +0300

    Esimene php lisatud

---
+++ esimene.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"> [andmed andmete kohta]
+	<title>Kaspar Aasmäe programeerib veebi</title>
+</head>
+<body>
+	<h1>Kaspar Aasmäe, veebiprogrammeerimine<h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<?php
+	echo "Algas PHP õppimine.";
+	?>
+</body>
+</html>
