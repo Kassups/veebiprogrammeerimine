56d2b6e3639e43293d3167e8fd0fba4c7647db88
Kaspar Aasmäe <Kassups@users.noreply.github.com>
2017-09-21 11:46:21 +0300

    Piltide lisamine

---
+++ Tund3/avaleht.php
@@ -0,0 +1,24 @@
+<?php
+	//muutujad ja muus siuke
+	$myName = "Kaspar";
+	$myFamilyName = "Aasmäe";
+	
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"> 
+	<title>
+	Kaspar Aasmäe programeerib veebi
+	</title>
+</head>
+<body>
+	<h1><?php echo $myName ." " .$myFamilyName; ?>, veebiprogrammeerimine</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Meie õpime <a href="http://www.tlu.ee">Tallinna ülikoolis</a>.</p>
+	<p>Minu esimne php leht on <a href="../esimene.php">siin</a>.</p>
+	<p>Minu sõbra Meelise leht on <a href="../../../~lutsmeel/Veebiprogrammeerimine">siin</a>.</p>
+</body>
+</html>
--- esimene.php
+++ esimene.php
@@ -5,7 +5,7 @@
 	$monthNamesEt = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
 	//var_dump($monthNamesEt);
 	//echo $monthNamesEt[8];
-	$montNow = $monthNamesEt[date("n") - 1];
+	$monthNow = $monthNamesEt[date("n") - 1];
 	
 	//hindan päeva osa (võrdlemine < > <= >= == != )
 	$hourNow = date("H");
@@ -33,7 +33,7 @@
 		
 		$ageNotice .= "<p>Olete elanud järgnevatel aastatel:</p> <ol>";
 		for ($i = $myBirtYear; $i <= date("Y"); $i ++){
-			$ageNotice .= "<li>" .$ ."/li>";
+			$ageNotice .= "<li>" .$i ."</li>";
 		}
 		$ageNotice .="</ol>";
 			
@@ -61,12 +61,12 @@
 		echo "<p>Algas PHP õmine.</p>";
 		echo "<p>Täna on ";
 		echo date("d. ") .$monthNow .date(" Y")  .", kell oli lehe avamise hetkel " .date("H:i:s");
-		echo ", hetkel on " $partOfDay .".</p>";
+		echo ", hetkel on " .$partOfDay .".</p>";
 	?>
 	<h2>Natuke vanusest</h2>
 	<form method="Post">
 		<label>Teie sünniaasta: </label>
-		<input name="birthYear" id="birthYear" type="number" value="<?php echo $myBirtYear; ?> min="1900" max="2017">
+		<input name="birthYear" id="birthYear" type="number" value="<?php echo $myBirtYear; ?>" min="1900" max="2017">
 		<input name="submitBirthYear" type="submit" value="Sisesta">
 	</form>
 	<?php
